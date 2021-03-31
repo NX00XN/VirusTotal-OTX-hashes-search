a1f089b3ad7d3fc86dce46762eeca406254e0f8f
Nx@00A <36732518+NX00XN@users.noreply.github.com>
2021-03-31 18:21:56 +0200

    Create VirusTotal_v3_comments.py

---
+++ VirusTotal_v3_comments.py
@@ -0,0 +1,31 @@
+import pprint
+import requests
+import json
+import argparse
+
+BASE_URL = 'https://www.virustotal.com/api/v3/'
+API_KEY = 'XYZ' #Insert here API KEY
+
+# Information about files
+# '/files/{hash}'
+def get_file_report(hash):
+    url = 'files/' + hash + '/comments'
+    headers = {'x-apikey': API_KEY}
+    response = requests.get(BASE_URL + url, headers=headers)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        response_json = response.json()
+        error = response_json['error']['code'] + ' - ' + response_json['error']['message']
+        return error
+#  main
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Add hash.')
+    parser.add_argument('--file', help='foo help')
+    args = parser.parse_args()
+
+    result = get_file_report(args.file)
+    print(json.dumps(result)) #json print
+
